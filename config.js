85990737aae0128e46c44eaf801e0ae0c958ec2a
blackhat317 <ayeshakhanseo786@gmail.com>
2025-07-19 12:15:08 -0700

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "IK~QfMl2BhD#nvrv1v9KVJ2P1ueqHdRrC1j4lOp80TfmyrYJAWzWp8Q",
+SESSION_ID: process.env.SESSION_ID || "IK~4D9XTbjD#K7Bp3ZLr2JFpguXWNENmasnzWid0wT2GBvHnpsBokJs",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -37,7 +37,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "true",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "923191327275",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "923276811525",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "Jᴀᴡᴀᴅ TᴇᴄʜX",
 // add bot owner name
